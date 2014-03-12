b695c00fa77a8969254f6e07709490cb3f1fed8f
hunt <hunterbrennick@gmail.com>
2014-03-11 20:52:04 -0400

    nav bar, checkout btn trigger

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 
 $(document).ready(function() {
-    
+
+    /* throw checkout */
+    $("#checkout-trig").click(function() {
+        //window.location.href = "http://localhost:63342/gramps_v1/checkout.html";
+        window.location.replace("http://localhost:63342/gramps_v1/checkout.html");
+    });
 
 
 
